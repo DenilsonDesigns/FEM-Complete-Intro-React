0d8de7282fc9ec6da4821fa0c47ddb9dfb167c53
DenilsonDesigns <daniellanguilller@gmail.com>
2019-01-19 21:00:39 +1100

    testing done

--- src/Details.js
+++ src/Details.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import pf from "petfinder-client";
 import { navigate } from "@reach/router";
 import Carousel from "./Carousel";
@@ -9,80 +9,71 @@ const petfinder = pf({
   secret: process.env.API_SECRET
 });
 
-class Details extends Component {
-  state = {
-    loading: true,
-    showModal: true
-  };
-
-  toggleModal = () => {
-    this.setState({
-      showModal: !this.state.showModal
-    });
-  };
-
+class Details extends React.Component {
+  state = { loading: true, showModal: false };
   componentDidMount() {
     petfinder.pet
       .get({
         output: "full",
         id: this.props.id
       })
-      .then(res => {
-        const pet = res.petfinder.pet;
+      .then(data => {
         let breed;
-        if (Array.isArray(pet.breeds.breed)) {
-          breed = pet.breeds.breed.join(", ");
+        if (Array.isArray(data.petfinder.pet.breeds.breed)) {
+          breed = data.petfinder.pet.breeds.breed.join(", ");
         } else {
-          breed = pet.breeds.breed;
+          breed = data.petfinder.pet.breeds.breed;
         }
-
         this.setState({
-          name: pet.name,
-          animal: pet.animal,
-          location: `${pet.contact.city}, ${pet.contact.state}`,
-          description: pet.description,
-          media: pet.media,
+          name: data.petfinder.pet.name,
+          animal: data.petfinder.pet.animal,
+          location: `${data.petfinder.pet.contact.city}, ${
+            data.petfinder.pet.contact.state
+          }`,
+          description: data.petfinder.pet.description,
+          media: data.petfinder.pet.media,
           breed,
           loading: false
         });
       })
-      .catch(err => {
+      .catch(() => {
         navigate("/");
       });
   }
-
+  toggleModal = () => this.setState({ showModal: !this.state.showModal });
   render() {
     if (this.state.loading) {
-      return <h1>Loading...</h1>;
+      return <h1>loading … </h1>;
     }
 
     const {
-      name,
+      media,
       animal,
       breed,
       location,
       description,
-      media,
+      name,
       showModal
     } = this.state;
+
     return (
       <div className="details">
         <Carousel media={media} />
-        <h1>{name}</h1>
-        <h2>
-          {animal} - {breed} - {location}
-        </h2>
-        <button onClick={this.toggleModal}>Adopt {name}</button>
-        <p>{description}</p>
-        {showModal ? (
-          <Modal>
-            <h1>Would you like to adopt {name}</h1>
-            <div className="buttons">
-              <button onClick={this.toggleModal}>Yes</button>
-              <button onClick={this.toggleModal}>Hell Yes</button>
-            </div>
-          </Modal>
-        ) : null}
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} — ${breed} — ${location}`}</h2>
+          <button onClick={this.toggleModal}>Adopt {name}</button>
+          <p>{description}</p>
+          {showModal ? (
+            <Modal>
+              <h1>Would you like to adopt {name}?</h1>
+              <div className="buttons">
+                <button onClick={this.toggleModal}>Yes</button>
+                <button onClick={this.toggleModal}>No</button>
+              </div>
+            </Modal>
+          ) : null}
+        </div>
       </div>
     );
   }
