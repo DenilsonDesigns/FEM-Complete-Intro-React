f9d528c01d6c6d10338f44586475a00582bd32a5
DenilsonDesigns <daniellanguilller@gmail.com>
2019-01-19 22:13:59 +1100

    finished emotion

---
+++ src/Navbar.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Link } from "@reach/router";
+import styled from "@emotion/styled";
+import colors from "./colors";
+import { keyframes } from "@emotion/core";
+
+const Spin = keyframes`
+  from {
+    transform: rotate(0deg)
+  }
+
+  to{
+    transform: rotate(360deg)
+  }
+`;
+
+const SpyGlass = styled("span")`
+  display: inline-block;
+  animation: 1s ${Spin} linear infinite;
+`;
+
+const Container = styled("header")`
+  background-color: ${colors.dark};
+  position: sticky;
+  top: 0;
+  z-index: 10;
+`;
+
+const NavLink = styled(Link)`
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const Navbar = () => {
+  return (
+    <Container>
+      <NavLink to="/">Adopt Me!</NavLink>
+      <NavLink to="/search-params">
+        <SpyGlass aria-label="search" role="img">
+          🔍
+        </SpyGlass>
+      </NavLink>
+    </Container>
+  );
+};
+
+export default Navbar;
